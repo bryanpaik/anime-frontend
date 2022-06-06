d4f3594809734c722fecbb86671916bcc6490bb3
Bryan Paik <74121118+bryanpaik@users.noreply.github.com>
2022-06-05 20:31:37 -0400

    Revert "topbar redo"
    This reverts commit bd4662689e545e9762df4694169fd71feeec35ad.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopbarComponent } from './topbar/topbar.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TopbarComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
