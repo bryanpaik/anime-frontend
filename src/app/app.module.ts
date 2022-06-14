6d95a3f85fdfcf6f36a8b09ac52b93100ee059ee
Bryan Paik <74121118+bryanpaik@users.noreply.github.com>
2022-06-14 19:32:46 -0400

    Video player refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { VideoPlayerComponent } from './utils/video-player/video-player.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
