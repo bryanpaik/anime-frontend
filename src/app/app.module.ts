f5b758c1b6b30733a150074dfae96b5e6b20af9c
Bryan Paik <74121118+bryanpaik@users.noreply.github.com>
2022-06-14 18:53:28 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule,  } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
