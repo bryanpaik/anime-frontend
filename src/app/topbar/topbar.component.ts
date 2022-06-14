32b6e44cd640bc76835e6992e0059b867d9ff6e9
byron-mejia-castillo <bymejia11@gmail.com>
2022-06-14 15:22:38 -0400

    topbar

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'shit-monkey',
+  selector: 'topbar-nav',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.scss']
 })
