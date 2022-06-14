e488472bf2543a7c5b2017fb485dbec1ab3620a7
Bryan Paik <74121118+bryanpaik@users.noreply.github.com>
2022-06-14 18:56:40 -0400

    Revert "Revert merge"
    This reverts commit 482e3877fa48dd88786ebfccf391ce46fd7ccde8.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnimeApiService } from './anime-api.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private animeApi: AnimeApiService) {}
+
   title = 'anime-frontend';
+
+  ngOnInit(){
+    this.animeApi.getStatus();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule,  } from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './home/home.component';
 import { FriendsComponent } from './friends/friends.component';
