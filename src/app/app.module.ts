4b0ec7c16dfb76c67a5ffc3aa7d3b4a679f1bc01
Bryan Paik <74121118+bryanpaik@users.noreply.github.com>
2022-06-14 18:44:39 -0400

    Revert "Merge pull request #6 from bryanpaik/top-bar"
    This reverts commit 869f43b179b7da7c0604b6adf98d1950ec48732c, reversing
    changes made to f00aa449c6069391ba4d81cbf197257813f0d169.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule, routingComponents} from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { HomeComponent } from './home/home.component';
-import { FriendsComponent } from './friends/friends.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { SearchComponent } from './search/search.component';
-import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    routingComponents,
-    HomeComponent,
-    FriendsComponent,
-    CategoriesComponent,
-    SearchComponent,
-    SettingsComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
