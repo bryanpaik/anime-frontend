90eb89f9a3b9c3ce70365147dbb18b0077da0562
Bryan Paik <74121118+bryanpaik@users.noreply.github.com>
2022-10-11 22:48:06 -0400

    deprecate-login

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { FireBaseService } from 'src/app/utils/services/fire-base.service';
-import { CreateAccountComponent } from '../create-account/create-account.component';
-import { LoginComponent } from '../login/login.component';
+// import { CreateAccountComponent } from '../create-account/create-account.component';
+// import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'topbar-nav',
@@ -10,39 +10,39 @@ import { LoginComponent } from '../login/login.component';
   styleUrls: ['./topbar.component.scss']
 })
 export class TopbarComponent implements OnInit {
-  loggedIn: boolean = false;
+  // loggedIn: boolean = false;
 
   constructor(public dialog: MatDialog, public fireBase: FireBaseService) { }
 
   ngOnInit(): void {
   }
 
-  login(): void {
-    const login = this.dialog.open(LoginComponent);
-    login.afterClosed().subscribe(result => {
-      this.fireBase.getCredentials().then(res => {
-        if(res) {
-          this.loggedIn = true;
-        }
-      });
-    });
+  // login(): void {
+  //   const login = this.dialog.open(LoginComponent);
+  //   login.afterClosed().subscribe(result => {
+  //     this.fireBase.getCredentials().then(res => {
+  //       if(res) {
+  //         this.loggedIn = true;
+  //       }
+  //     });
+  //   });
 
-  }
+  // }
 
-  logout() {
-    this.loggedIn = false;
-    this.fireBase.logout();
-  }
+  // logout() {
+  //   this.loggedIn = false;
+  //   this.fireBase.logout();
+  // }
 
-  createAccount(): void {
-    const account = this.dialog.open(CreateAccountComponent);
-    account.afterClosed().subscribe(result => {
-      this.fireBase.getCredentials().then(res => {
-        if(res) {
-          this.loggedIn = true;
-        }
-      });
-    });
-  }
+  // createAccount(): void {
+  //   const account = this.dialog.open(CreateAccountComponent);
+  //   account.afterClosed().subscribe(result => {
+  //     this.fireBase.getCredentials().then(res => {
+  //       if(res) {
+  //         this.loggedIn = true;
+  //       }
+  //     });
+  //   });
+  // }
 
 }
